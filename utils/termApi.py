6a70f151ed472d0a2b50508d8c4f05640c4859f4
lonely-v3n1x <karimyussif63@gmail.com>
2020-11-23 19:56:50 +0000

    Added log runcmd and Data methods to the modules and tested[works]

--- utils/termApi.py
+++ utils/termApi.py
@@ -22,24 +22,44 @@ class deviceApi(object):
     def __self__():
         self.sp = subprocess
 
+    # log errors
+    def log(self, msg):
+        log = logging.getLogger("TermApi")
+        log.error(msg)
+        console.print(msg, style="bold red")
+
+    # Method to run cmds
+    def runcmd(self, cmd: list):
+        return sp.run(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
+
+    def Data(self):
+        try:
+            getData = self.runcmd(["termux-telephony-deviceinfo"])
+            res = json.loads(getData.stdout)
+            # print(res["data_state"])
+
+            if res["data_state"] == "connected":
+                return True
+            else:
+                return False
+        except:
+            self.log("Error accessing termux-telephony-deviceinfo")
+
     def getHeadsetInfo(self):
         try:
-            results = sp.run(["termux-audio-info"], stdout=sp.PIPE, stderr=sp.PIPE)
-
-            res = json.loads(results.stdout)
-
+            headsetapi = self.runcmd(["termux-audio-info"])
+            res = json.loads(headsetapi.stdout)
             # For debugging
             # print(res["WIREDHEADSET_IS_CONNECTED"])
 
             return res["WIREDHEADSET_IS_CONNECTED"]
-
         except FileNotFoundError:
-            log = logging.getLogger("TermApi")
-            log.error("Errors with termux-audio-info")
-            console.print("Problem with accessing termux-audio-info", style="bold red")
-            exit()
+            self.log("Errorf accessing termux-audio-info")
 
 
 if __name__ == "__main__":
     test = deviceApi()
-    test.getHeadsetInfo()
+    if test.Data():
+        print("Dgood")
+    if test.getHeadsetInfo():
+        print("good")
