b991f5f3501b59e71206e460637653bf3cfe80cc
lonely-v3n1x <karimyussif63@gmail.com>
2020-11-29 22:08:49 +0000

    Created device info method

--- api/base.py
+++ api/base.py
@@ -1,10 +1,30 @@
 import subprocess as sp
 
-api='/data/data/com.termux/files/usr/libexec/termux-api'
 
-def runcmd(tp):
-    return sp.run([api,tp],stdout=sp.PIPE, stderr=sp.PIPE)
+class BaseApi():
+    def __init__(self):
+        self.api=\
+            '/data/data/com.termux/files/usr/libexec/termux-api'
 
-ru=runcmd('TelephonyDeviceInfo')
+    def runcmd(self,cmd):
+        try:
+            return sp.run(
+                [cmd],stdout=sp.PIPE, stderr=sp.PIPE)
+        except:
+            print(f'Error with {cmd}')
+            return None
 
-print(ru.stdout)
+    def termapi(self,typ,arg=''):
+        try:
+            return sp.run(
+                [self.api,typ,arg],stdout=sp.PIPE, stderr=sp.PIPE)
+        except:
+            print(f'Error with {typ}{arg}')
+
+    #Returns info about device
+    def deviceInfo(self):
+        return self.termapi('TelephonyDeviceInfo')
+
+if __name__ == "__main__":
+    test=BaseApi()
+    print(test.deviceInfo().stdout)
