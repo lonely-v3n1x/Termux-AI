227e5dea6b7a0bee0363bfead2e5159de6980e4c
lonely-v3n1x <karimyussif63@gmail.com>
2020-11-29 21:34:56 +0000

    trying the new idea

--- api/base.py
+++ api/base.py
@@ -1,86 +1,10 @@
-import rich
 import subprocess as sp
-from rich.console import Console
-import validators
-try:
-    from func import parse,uri_validator
-except :
-    '''
-    print('load not found')
-    quit()
-    '''
-    from ujson import load,loads
 
+api='/data/data/com.termux/files/usr/libexec/termux-api'
 
-# Rich module Console init
-console = Console()
+def runcmd(tp):
+    return sp.run([api,tp],stdout=sp.PIPE, stderr=sp.PIPE)
 
-class BaseApi(object):
-    def __self__():
-        self.sp = subprocess
+ru=runcmd('TelephonyDeviceInfo')
 
-    # Method to run cmds
-    def runcmd(self, cmd: list):
-        try:
-            return sp.run(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
-        except:
-            print(f"Error with {cmd}")
-    def deviceInfo(self):
-        return loads(
-            self.runcmd(["termux-telephony-deviceinfo"]).stdout)
-
-    def audioInfo(self):
-        return loads(self.runcmd(["termux-audio-info"]).stdout)
-
-    def batteryStatus(self):
-        return loads(
-            self.runcmd(["termux-battery-status"]).stdout)
-
-    def brightness(self,num):
-        if (num > 0 and num < 255):
-            sp.run(['termux-brightness', str(num) ])
-            return True
-        return False
-
-    #Use arg get=True to return json of volume data
-    def volume(self,stream,vol :int):
-        streams=('alarm', 'music',
-                 'notification', 'ring', 'system', 'call')
-        if stream in streams:
-            sp.run(['termux-volume',stream,str(vol)])
-        return False
-
-    def clipboard(self,get=False,st=False,inp=''):
-        if get and not st:
-            return self.runcmd(["termux-clipboard-get"]).stdout
-        else:
-            self.runcmd(["termux-clipboard-set",f"{inp}"])
-
-
-    def callLog(self):
-        return loads(
-            self.runcmd(["termux-call-log"]).stdout)
-
-    def contactList(self):
-        return loads(
-            self.runcmd(["termux-contact-list"]).stdout)
-
-    def url_open(self,url):
-        if validators.url(url):
-            sp.run([f'termux-open-url',url])
-            return True
-        return False
-
-    def toast(self, text, bg="black", cl="white", pos="top"):
-        doc = """
-        bg for background color ,cl for text color, pos for postion [top, middle, or bottom]
-        """
-        try:
-            sp.run(
-                [f"termux-toast -b{bg} -c{cl} -g{pos} {text}"], shell=True)
-        except:
-            self.log("Error acessing termux-toast")
-
-
-if __name__ == "__main__":
-    test = BaseApi()
+print(ru.stdout)
